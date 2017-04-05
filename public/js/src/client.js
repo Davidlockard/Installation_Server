c0d32eae56153e9f62632a50f6c7f5c52327789d
itp student <itpstudent@172-16-239-11.DYNAPOOL.NYU.EDU>
2017-04-05 00:22:52 -0400

    is this working?

--- public/js/src/client.js
+++ public/js/src/client.js
@@ -13,7 +13,7 @@ $(document).ready(function(){
     var keys = Object.keys(data);
     $('#gif_container').fadeOut(250, function(){
       $('#hello_name').empty().append('Hello ' + data.name);
-      $('#gif_container').empty().append('<img src="assets/gifs/' + data[keys[0]] + '.gif" width="1024" height="768" />')
+      $('#gif_container').empty().append('<img src="assets/gifs/' + data[keys[1][0]] + '.gif" width="1024" height="768" />')
       $('#gif_container').fadeIn(250);
     })
   })
