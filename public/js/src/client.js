d88857d9c6d3013a1d18178a71e56a7f2930257c
itp student <itpstudent@172-16-239-11.DYNAPOOL.NYU.EDU>
2017-04-04 23:40:29 -0400

    Adding .html to the client

--- public/js/src/client.js
+++ public/js/src/client.js
@@ -5,7 +5,7 @@ $(document).ready(function(){
   socket.on('connect', function(){
 
     //Push the landing GIF into the screen
-    $('#gif_container').empty().append('<img src="assets/gifs/lesson01.gif" width="1024" height="768" />');
+    $('#gif_container').empty().html('<img src="assets/gifs/lesson01.gif" width="1024" height="768" />');
 
   });
 
