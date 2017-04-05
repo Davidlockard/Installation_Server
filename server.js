bb4f50457436ed35331634d62cdbe871d4549eb3
itp student <itpstudent@172-16-239-11.DYNAPOOL.NYU.EDU>
2017-04-04 21:45:19 -0400

    Initial commit

---
+++ server.js
@@ -0,0 +1,53 @@
+'use strict'
+
+//Express setup
+const express = require('express');
+const app = express();
+
+//HTTP Server setup
+const http = require('http').createServer(app);
+
+//Port and listeners
+let port = process.env.PORT || 2308;
+let server = app.listen(port);
+
+//I hate templating, so I prefer ejs
+const ejs = require('ejs');
+
+//twilio
+let twilioCred = {
+  sid: 'AC0615aa63cbef5e775dad5668fee9be48',
+  auth: 'f919c2750049933dc2943d987dc2c089'
+}
+const twilio = require('twilio');
+let client = new twilio.RestClient(twilioCred.sid, twilioCred.auth);
+
+//App config
+app.set('views', __dirname + '/views');
+app.engine('.html', ejs.__express);
+app.set('view-engine', 'html');
+
+//Setup the public space
+app.use(express.static(__dirname + '/public'));
+
+console.log("The server is running on localhost:" + port);
+
+app.get('/projector', (req, res)=>{
+
+    res.render('projector.html');
+
+    client.messages.create({
+    body: 'Shalom from our node server',
+    to: '+19172468365',  // Text this number
+    from: '+12245151972' // From a valid Twilio number
+    }, function(err, message) {
+        console.log(message.sid);
+    });
+
+});
+
+app.get('/*', (req, res)=>{
+
+    res.render('404.html')
+
+})
